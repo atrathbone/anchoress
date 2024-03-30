e9fad811e8c651a464aac02024b82970d3419ace
atrathbone <alexander.thomas.rathbone@hotmail.com>
2024-03-30 15:55:53 +0100

    set up scroll point

--- src/app/home/pages/epk/epk.component.ts
+++ src/app/home/pages/epk/epk.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { ResponsiveService } from 'src/app/responsive.service';
+import { IconLink } from 'src/app/types/types';
 
 @Component({
   selector: 'app-epk',
-  standalone: true,
-  imports: [],
   templateUrl: './epk.component.html',
   styleUrl: './epk.component.scss'
 })
 export class EpkComponent {
+  resService : ResponsiveService;
+  iconLinks: IconLink[]= [{
+    //iconUrl:"assets/images/logos/bandcamp-logo.svg.png",
+    text: "BANDCAMP",
+    linkUrl:"https://anchoress-official.bandcamp.com/album/in-times-where-eyes-are-low"
+  },
+  {
+    //iconUrl:"assets/images/logos/insta-logo.svg.png",
+    text:"INSTAGRAM",
+    linkUrl:"https://www.instagram.com/anchoressss/"
+  },
+  {
+    //iconUrl:"assets/images/logos/soundcloud-logo.png",
+    text:"SOUNDCLOUD",
+    linkUrl:"https://soundcloud.com/anchoress-berlin"
+  },]
+
+
+  constructor(private responsive: ResponsiveService) {
+    this.resService = responsive;
+   }
 
 }
--- src/app/home/pages/pages.module.ts
+++ src/app/home/pages/pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeDisplayComponent } from './home-display/home-display.component';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { EpkComponent } from './epk/epk.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeDisplayComponent
+    HomeDisplayComponent,
+    EpkComponent
   ],
   imports: [
     CommonModule,
