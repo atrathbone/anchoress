8a3b0720ef58f19a24300a33907fcbe760648380
atrathbone <alexander.thomas.rathbone@hotmail.com>
2024-04-10 11:19:34 +0100

    some small bits

--- src/app/home/pages/home-display/home-display.component.ts
+++ src/app/home/pages/home-display/home-display.component.ts
@@ -11,58 +11,58 @@ export class HomeDisplayComponent {
   resService : ResponsiveService;
   liveDates: LiveDate[] = [
     {
-      date: new Date("03-02-2023"),
+      date: new Date(this.getCompatableDateStr("03-02-2023")),
       venue: "PAS",
       location: "Berlin (DE)",
       link: "https://pas-berlin.org/"
     },
     {
-      date: new Date("04-20-2023"),
+      date: new Date(this.getCompatableDateStr("04-20-2023")),
       venue: "8mm Bar",
       location: "Berlin (DE)",
       link: "https://www.eventim-light.com/de/a/5f002783c9f8d46492128f51/e/64243cb005c38b0a07ff055e"
     },
     {
-      date: new Date("05-13-2023"),
+      date: new Date(this.getCompatableDateStr("05-13-2023")),
       venue: "(NYT)",
       location: "Berlin (DE)",
       notes: "solo sets",
       link: "https://www.instagram.com/not.yet.titled.art.space/"
     },
     {
-      date: new Date("06-21-2023"),
+      date: new Date(this.getCompatableDateStr("06-21-2023")),
       venue: "Loophole",
       location: "Berlin (DE)",
       link: "https://www.instagram.com/loophole_brln/?hl=de"
     },
     {
-      date: new Date("07-14-2023"),
+      date: new Date(this.getCompatableDateStr("07-14-2023")),
       venue: "PAS",
       location: "Berlin (DE)",
       notes: "solo sets",
       link: "https://pas-berlin.org/"
     },
     {
-      date: new Date("09-07-2023"),
+      date: new Date(this.getCompatableDateStr("09-07-2023")),
       venue: "Arkaoda",
       location: "Berlin (DE)",
       link: "https://berlin.arkaoda.com/?/default/detail/id=830"
     },
     {
-      date: new Date("09-23-2023"),
+      date: new Date(this.getCompatableDateStr("09-23-2023")),
       venue: "Cittipunkt",
       location: "Berlin (DE)",
       notes: "solo sets",
       link: "http://cittipunkt.online/newsletter.pdf"
     },
     {
-      date: new Date("10-19-2023"),
+      date: new Date(this.getCompatableDateStr("10-19-2023")),
       venue: "KW Institute/Pogo Bar",
       location: "Berlin (DE)",
       link: "https://www.kw-berlin.de/pogo-bar-cockdust/"
     },
   {
-    date: new Date("03-08-2024"), 
+    date: new Date(this.getCompatableDateStr("03-08-2024")), 
     venue: "Zwölf Apostel Kirche",
     location: "Berlin (DE)",
     link: "https://t.rausgegangen.de/tickets/angry-women-joanna-gemma-auguri-hazel-iris-anchoress-aniqo"
@@ -86,6 +86,10 @@ export class HomeDisplayComponent {
 
   constructor(private responsive: ResponsiveService) {
     this.resService = responsive;
+   }
+
+   getCompatableDateStr(dString: string){
+    return dString.replace(/-/g, "/")
    }
 
 }
