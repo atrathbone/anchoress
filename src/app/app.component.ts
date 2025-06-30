2968ff8439a65b2f8964f6391c5005fdc7f8513f
EITCO\ARathbone <arathbone@eitco.de>
2025-06-30 19:47:29 +0200

    try some chatgpt wizardry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'anchoress';
+
+  ngOnInit(): void {
+    this.setVh();
+  }
+
+  @HostListener('window:resize')
+  @HostListener('window:orientationchange')
+  onResize() {
+    this.setVh();
+  }
+
+  setVh() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 }
