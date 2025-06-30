0cceebd638a3070c7ad358ff40ce5caafa9ed6f6
EITCO\ARathbone <arathbone@eitco.de>
2025-06-30 19:27:38 +0200

    add vid

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, Renderer2, ViewChild, ViewEncapsulation} from '@angular/core';
 import {ResponsiveService} from '../responsive.service';
 
 @Component({
@@ -8,11 +8,23 @@ import {ResponsiveService} from '../responsive.service';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent {
+  @ViewChild("bgVideo") bgVideo!: ElementRef<HTMLVideoElement>;
 
   public resService: ResponsiveService;
 
-  constructor(responsive: ResponsiveService) {
+  constructor(responsive: ResponsiveService, private renderer: Renderer2) {
     this.resService = responsive;
+  }
+
+  ngAfterViewInit() {
+    this.renderer.listen('body', 'click', () => {
+      const video = this.bgVideo.nativeElement;
+      if (video.paused) {
+        video.play().catch(() => {
+          console.warn('Video plavback blocked!');
+        });
+      }
+    });
   }
 
 }
